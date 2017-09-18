63b091ad90421975104c4d57e795488235d0143a
lilei <122024832@qq.com>
2017-09-18 16:46:40 +0800

    9-18尺寸适配

--- app/src/main/java/box/lilei/box_client/client/adapter/GvHomeGoodsAdapter.java
+++ app/src/main/java/box/lilei/box_client/client/adapter/GvHomeGoodsAdapter.java
@@ -28,14 +28,43 @@ public class GvHomeGoodsAdapter extends MyBaseAdapter<Goods> {
         mContext = context;
     }
 
+    public View saleStateView;
+
     @Override
     protected void convert(Goods goods, MyViewHolder viewHolder,int position) {
         ((ImageView) viewHolder.getView(R.id.adbanner_b_item_img)).setImageResource(R.drawable.goods_glc_dft);
         if(position%2==0){
+            //促销
+            goodsSaleState(viewHolder,1);
+
+            ((ImageView)viewHolder.getView(R.id.adbanner_b_item_img_wd)).setImageResource(R.mipmap.logo_cold);
             viewHolder.getView(R.id.adbanner_b_item_bg_rl).setBackgroundColor(ContextCompat.getColor(mContext,R.color.colorHomeGoodsBg));
+
         }else{
+            //售罄
+            goodsSaleState(viewHolder,2);
             viewHolder.getView(R.id.adbanner_b_item_bg_rl).setBackgroundColor(Color.WHITE);
+            ((ImageView)viewHolder.getView(R.id.adbanner_b_item_img_wd)).setImageResource(R.mipmap.logo_hot);
         }
 //        ((TextView)viewHolder.getView(R.id.adbanner_b_item_price)).setText(goods.getGoodsName());
     }
+
+    public void goodsSaleState(MyViewHolder viewHolder, int state){
+        saleStateView = viewHolder.getView(R.id.adbanner_b_item_rl_sale_state);
+        if(state == 1) {
+            saleStateView.setVisibility(View.VISIBLE);
+            saleStateView.setBackgroundResource(R.drawable.shape_sale_state_discount);
+            viewHolder.getView(R.id.adbanner_b_item_txt_sale_out).setVisibility(View.INVISIBLE);
+        }else if(state == 2){
+            saleStateView.setVisibility(View.VISIBLE);
+            saleStateView.setBackgroundResource(R.drawable.shape_sale_state_off);
+            viewHolder.getView(R.id.adbanner_b_item_txt_sale_out).setVisibility(View.VISIBLE);
+            viewHolder.getView(R.id.adbanner_b_item_txt_sale).setVisibility(View.INVISIBLE);
+            viewHolder.getView(R.id.adbanner_b_item_discount_rmb).setVisibility(View.INVISIBLE);
+            viewHolder.getView(R.id.adbanner_b_item_txt_discount_price).setVisibility(View.INVISIBLE);
+        }else{
+
+        }
+    }
+
 }
