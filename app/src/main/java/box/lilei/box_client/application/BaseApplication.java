9fc2768525a74f11e876c1288d67602f2b1eff4c
lilei <122024832@qq.com>
2017-11-28 19:27:18 +0800

    11-27温度和省电模式设置

--- app/src/main/java/box/lilei/box_client/application/BaseApplication.java
+++ app/src/main/java/box/lilei/box_client/application/BaseApplication.java
@@ -48,7 +48,7 @@ public class BaseApplication extends Application {
 
         //初始化异常捕捉
         ExceptionHandler exceptionHandler = ExceptionHandler.getInstance();
-        exceptionHandler.init(this);
+//        exceptionHandler.init(this);
     }
 
     private void initSDKiniFile() {
