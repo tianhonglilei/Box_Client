6fba749a90efa9e533d35e8fa19867016fd68848
lilei <122024832@qq.com>
2017-11-30 17:58:37 +0800

    11-29出货

--- app/src/main/java/box/lilei/box_client/client/view/activity/PayActivity.java
+++ app/src/main/java/box/lilei/box_client/client/view/activity/PayActivity.java
@@ -454,7 +454,7 @@ public class PayActivity extends Activity implements View.OnClickListener, PayVi
         window.setFocusable(false);
         window.setOutsideTouchable(false);
         changePopwindowBg(0.7f);
-        window.showAsDropDown(mainView, 0, 0, Gravity.CENTER);
+        window.showAsDropDown(mainView, 0, 0);
         new CountDownTimer(3000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
