e24d390fdaf8f5c0b5d332b61b0a56a933185d2a
lilei <122024832@qq.com>
2017-11-30 15:06:10 +0800

    11-29出货

--- app/src/main/java/box/lilei/box_client/client/receiver/GoodsBroadcastReceiver.java
+++ app/src/main/java/box/lilei/box_client/client/receiver/GoodsBroadcastReceiver.java
@@ -18,6 +18,7 @@ public class GoodsBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.e("GoodsBroadcastReceiver", intent.getAction());
         if (intent.getAction().equals(BoxAction.OUT_GOODS_RECEIVER_ACTION)) {
             int state = BoxAction.getOutGoodsState();
             Log.e("GoodsBroadcastReceiver", "state:" + state);
