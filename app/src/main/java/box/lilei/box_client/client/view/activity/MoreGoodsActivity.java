67329437b86eb966e5ec68d99a79d4a8b987d48d
lilei <122024832@qq.com>
2017-12-01 19:04:16 +0800

    12-1

--- app/src/main/java/box/lilei/box_client/client/view/activity/MoreGoodsActivity.java
+++ app/src/main/java/box/lilei/box_client/client/view/activity/MoreGoodsActivity.java
@@ -375,13 +375,13 @@ public class MoreGoodsActivity extends Activity implements View.OnClickListener,
                 public void run() {
                     moreGoodsGv.smoothScrollBy(moreGoodsGv.getMeasuredHeight(),1000);
                 }
-            },1500);
+            },2000);
             new Timer().schedule(new TimerTask() {
                 @Override
                 public void run() {
                     moreGoodsGv.smoothScrollBy(-moreGoodsGv.getHeight(),1000);
                 }
-            },3000);
+            },3500);
     }
 
 }
